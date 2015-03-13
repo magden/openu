c8f45defe31a0456b3667ccf6261746a7ca67720
Stas <stasel@gmail.com>
2015-03-13 14:59:13 +0200

    20554 Maman 11 ex 2

---
+++ 20554 Advanced Java/Maman11/ex2/src/GameOfLife.java
@@ -0,0 +1,136 @@
+/**
+ * Created by Stas on 13/03/2015.
+ */
+public class GameOfLife
+{
+    private Boolean[][] currentGen;
+    private Boolean[][] nextGen;
+
+    public GameOfLife(int height, int width)
+    {
+        currentGen = new Boolean[height][width];
+        for (int i = 0; i < currentGen.length; i++)
+        {
+            for (int j = 0; j < currentGen[i].length; j++)
+            {
+                currentGen[i][j] = false;
+            }
+        }
+    }
+
+    public void setCellState(int row, int col, Boolean state)
+    {
+        if (isValidCell(col, row))
+        {
+            currentGen[row][col] = state;
+        }
+    }
+
+    private Boolean isValidCell(int row, int col)
+    {
+        return row >= 0 && row < currentGen.length &&
+                col >= 0 && col < currentGen[row].length;
+    }
+
+    public void nextGeneration()
+    {
+        nextGen = currentGen.clone();
+
+        for (int i = 0; i < currentGen.length; i++)
+        {
+            for (int j = 0; j < currentGen[i].length; j++)
+            {
+                int neighbors = livingNeighbors(i, j);
+
+                //birth
+                if (!currentGen[i][j] && neighbors == 3)
+                {
+                    nextGen[i][j] = true;
+                }
+
+                //life
+                else if (currentGen[i][j] && neighbors >= 2 && neighbors <= 3)
+                {
+                    nextGen[i][j] = true;
+                }
+
+                //death
+                else
+                {
+                    nextGen[i][j] = false;
+                }
+            }
+        }
+
+        currentGen = nextGen;
+    }
+
+    private int livingNeighbors(int row, int col)
+    {
+        int living = 0;
+
+        //Top row
+        if (isValidCell(row - 1, col - 1) && currentGen[row - 1][col - 1])
+        {
+            living++;
+        }
+
+        if (isValidCell(row - 1, col) && currentGen[row - 1][col])
+        {
+            living++;
+        }
+
+        if (isValidCell(row - 1, col + 1) && currentGen[row - 1][col + 1])
+        {
+            living++;
+        }
+
+
+        //Middle row
+        if (isValidCell(row, col - 1) && currentGen[row][col - 1])
+        {
+            living++;
+        }
+
+        if (isValidCell(row, col + 1) && currentGen[row][col + 1])
+        {
+            living++;
+        }
+
+
+        //Bottom Row
+        if (isValidCell(row + 1, col - 1) && currentGen[row + 1][col - 1])
+        {
+            living++;
+        }
+
+        if (isValidCell(row + 1, col) && currentGen[row + 1][col])
+        {
+            living++;
+        }
+
+        if (isValidCell(row + 1, col + 1) && currentGen[row + 1][col + 1])
+        {
+            living++;
+        }
+
+        return living;
+
+    }
+
+    @Override
+    public String toString()
+    {
+        String output = "";
+        for (int i = 0; i < currentGen.length; i++)
+        {
+            for (int j = 0; j < currentGen[i].length; j++)
+            {
+                output += currentGen[i][j] ? " + " : " - ";
+            }
+            output += " \n";
+        }
+
+        return output;
+    }
+}
--- 20554 Advanced Java/Maman11/ex2/src/Program.java
+++ 20554 Advanced Java/Maman11/ex2/src/Program.java
@@ -1,10 +1,61 @@
 import javax.swing.*;
+import java.util.Random;
 
 public class Program
 {
-    public static void main(String[] args) {
-        // write your code here
-        String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hi "+name,"Welcome",JOptionPane.INFORMATION_MESSAGE);
+    public static void main(String[] args)
+    {
+        Boolean shouldStartOver;
+        do
+        {
+            shouldStartOver = false;
+
+            //Input from the user
+            String inputWidth, inputHeight;
+            do
+            {
+                inputWidth = JOptionPane.showInputDialog("Enter matrix width");
+            }
+            while (inputWidth == null);
+
+            do
+            {
+                inputHeight = JOptionPane.showInputDialog("Enter matrix height");
+            }
+            while (inputHeight == null);
+
+            int width = Integer.parseInt(inputWidth);
+            int height = Integer.parseInt(inputHeight);
+
+            //init game of life
+            GameOfLife game = new GameOfLife(height, width);
+            Random randomizer = new Random();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Boolean randomBool = randomizer.nextBoolean();
+                    game.setCellState(i, j, randomBool);
+                }
+            }
+
+            //show and handle results
+            int dialogResult = JOptionPane.showConfirmDialog(null, game);
+
+            while (dialogResult == JOptionPane.YES_OPTION)
+            {
+                game.nextGeneration();
+                dialogResult = JOptionPane.showConfirmDialog(null, game);
+            }
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION)
+            {
+                int startOverResult = JOptionPane.showConfirmDialog(null, "Would you like to start over?", null, JOptionPane.YES_NO_OPTION);
+                shouldStartOver = startOverResult == JOptionPane.YES_OPTION;
+            }
+
+        }
+        while (shouldStartOver);
     }
 }
