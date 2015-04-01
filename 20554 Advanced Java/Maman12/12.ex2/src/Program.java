4acb26fa73d259d5a4cd6c234d876af3304edc5f
Stas <stasel@gmail.com>
2015-04-02 00:36:09 +0300

    20554 maman 12 ex 2 main: creating random shapes

--- 20554 Advanced Java/Maman12/12.ex2/src/Program.java
+++ 20554 Advanced Java/Maman12/12.ex2/src/Program.java
@@ -1,3 +1,7 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Stas on 01/04/2015.
  */
@@ -5,6 +9,33 @@ public class Program
 {
     public static void main(String args[])
     {
+        ArrayList<MyShape> shapes = createShapes();
+    }
 
+    private static ArrayList<MyShape> createShapes()
+    {
+        ArrayList<MyShape> shapes = new ArrayList<MyShape>();
+
+        MyLine line1 = new MyLine(randomInt(200), randomInt(200), randomInt(200), randomInt(200), Color.RED);
+        MyLine line2 = new MyLine(randomInt(200), randomInt(200), randomInt(200), randomInt(200), Color.GREEN);
+        MyRectangle rect1 = new MyRectangle(randomInt(200), randomInt(200), randomInt(200), randomInt(200), Color.ORANGE, true);
+        MyRectangle rect2 = new MyRectangle(randomInt(200), randomInt(200), randomInt(200), randomInt(200), Color.BLACK, false);
+        MyOval oval1 = new MyOval(randomInt(200), randomInt(200), randomInt(200), randomInt(200), Color.PINK, true);
+        MyOval oval2 = new MyOval(randomInt(200), randomInt(200), randomInt(200), randomInt(200), Color.GRAY, false);
+
+        shapes.add(line1);
+        shapes.add(line2);
+        shapes.add(rect1);
+        shapes.add(rect2);
+        shapes.add(oval1);
+        shapes.add(oval2);
+
+        return shapes;
+    }
+
+    public static int randomInt(int max)
+    {
+        Random random = new Random();
+        return random.nextInt(max);
     }
 }
